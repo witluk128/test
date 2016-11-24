1a27d12a4b8e64b4f8098228f85f377ef5b2a35d
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-24 10:30:21 +0100

    p update3

--- 1.ts
+++ 1.ts
@@ -3,5 +3,6 @@ $(() => {
 
     $('#main').append('<p id="p"></p>');
     $('#p').append('ala ma kota' + '</br>');
-    $('#p').append('ala ma kota2');
+    $('#p').append('ala ma kota2' + '</br>');
+    $('#p').append('ala ma kota3' + '</br>');
 })
