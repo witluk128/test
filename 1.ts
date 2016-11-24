efa30e35c760e1d953afd8a6bb5cfb388d950ea4
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-24 14:10:55 +0100

    add WCF call

--- 1.ts
+++ 1.ts
@@ -7,4 +7,29 @@ $(() => {
     $('#p').append('ala ma kota3' + '</br>');
     $('#p').append('ala ma kota4' + '</br>');
     $('#p').append('ala ma kota53' + '</br>');
+
+    $('#reload').on('click', () => reload());
+
+    reload();
 })
+
+var serviceUrl = "http://localhost/WebTest/ListService.svc";
+
+reload = () => {
+    $.ajax({
+        type: "GET",
+        url: serviceUrl + "/GetItems",
+        contentType: "application/json; charset=utf-8"
+    })
+        .done((response) => {
+            var $list = $('#uList');
+            
+            $list.empty();
+            response.GetItemsResult.List.forEach(item => {
+                $list.append("<li>" + item.Name + "</li>");                                
+            });            
+        })
+        .fail((error) => { alert("Error: " + error.status + " : " + error.responseText) });
+}
+
+
