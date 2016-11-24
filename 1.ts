51b2b37e45572414e6e24359839b2378f46dbad0
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-24 13:35:18 +0100

    test

--- 1.ts
+++ 1.ts
@@ -4,5 +4,13 @@ $(() => {
     $('#main').append('<p id="p"></p>');
     $('#p').append('ala ma kota' + '</br>');
     $('#p').append('ala ma kota2' + '</br>');
-    $('#p').append('ala ma kota3' + '</br>');
+    $('#p').append('ala ma kota33' + '</br>');    
+
+    /*
+    {
+    "key": "ctrl+s",
+    "command": "workbench.action.tasks.build",
+    "when": "editorTextFocus && editorLangId == 'typescript'"
+    }
+    */
 })
