3e09054ff683897f592e4d9c34c00f8ad00b6ae4
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-24 13:21:35 +0100

    formatting

--- 2.ts
+++ 2.ts
@@ -3,7 +3,7 @@ $(() => {
 
     $('#main').append('<p id="p2"></p>');
     $('#p2').append('roman ma rower' + '</br>');
-    $('#p2').append('roman ma rower2');        
-    $('#p2').append('conflict solved!');
+    $('#p2').append('roman ma rower2' + '</br>');        
+    $('#p2').append('conflict solved!' + '</br>');
     $('#p2').append('roman ma rower3' + '</br>');
 })
