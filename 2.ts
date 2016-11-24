a5d5aae4c769cd420a494ead9f1a1f96fd07e17f
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-24 11:02:04 +0100

    p2 local update2

--- 2.ts
+++ 2.ts
@@ -2,5 +2,6 @@ $(() => {
     $('#main').append('<h3>git: 2</h3>');
     
     $('#main').append('<p id="p2"></p>');
-    $('#p2').append('roman ma rower');    
+    $('#p2').append('roman ma rower' + '</br>');
+    $('#p2').append('roman ma rower2');        
 })
