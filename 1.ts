e508cbb858ed6fab77baa995d3e78080451ac54a
Lukasz Witkowski <Lukasz.Witkowski@smtsoftware.com>
2016-11-25 10:23:09 +0100

    add form

--- 1.ts
+++ 1.ts
@@ -4,13 +4,12 @@ $(() => {
     $('#main').append('<p id="p"></p>');
     $('#p').append('ala ma kota' + '</br>');
     $('#p').append('ala ma kota2' + '</br>');
-    $('#p').append('ala ma kota33' + '</br>');    
+    $('#p').append('ala ma kota33' + '</br>');
 
-    /*
-    {
-    "key": "ctrl+s",
-    "command": "workbench.action.tasks.build",
-    "when": "editorTextFocus && editorLangId == 'typescript'"
-    }
-    */
+    $('#form').on('submit', (ev) => {
+        var i1 = $('#i1').val();
+        var i2 = $('#i2').val();
+
+        $('#p3').append(i1 + " : " + i2 + "</br>");
+    });
 })
